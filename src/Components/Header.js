33ee4da8ffc5514d866d982d95701d040765a06a
Jonathan Abraham Cobos Ramirez <jonathan.cobos@outlook.es>
2024-08-27 16:25:50 -0600

    Added google fonts

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,10 +3,10 @@ export default function Header() {
   return (
     <div className="sm:bg-orange-300 flex justify-center py-3 bg-slate-400 ">
       <div className=" items-center flex flex-1 px-8">
-        <img src={logo} alt="Company Logo" className="w-20" />
+        <img src={logo} alt="Company Logo" className="w-16" />
         <p className="flex px-4">Drone company</p>
       </div>
-      <div className="flex flex-1 items-center">
+      <div className="flex flex-1 items-center font-sanz">
         <p className="flex-1 text-center">Home</p>
         <p className="flex-1 text-center">Services</p>
         <p className="flex-1 text-center">Resources</p>
