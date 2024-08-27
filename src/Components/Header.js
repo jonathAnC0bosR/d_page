8cd29198af4331d4d8e11a2b4da1006ad9b32927
Jonathan Abraham Cobos Ramirez <jonathan.cobos@outlook.es>
2024-08-27 16:14:25 -0600

    Added styling to the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,17 @@
 import logo from "../logo.jpg";
 export default function Header() {
   return (
-    <div className="flex justify-center">
-      <div className=" items-center flex flex-1 mt-8 ml-16">
+    <div className="sm:bg-orange-300 flex justify-center py-3 bg-slate-400 ">
+      <div className=" items-center flex flex-1 px-8">
         <img src={logo} alt="Company Logo" className="w-20" />
-        <p className="flex ml-6">Drone company</p>
+        <p className="flex px-4">Drone company</p>
       </div>
-      <div className="flex flex-1 mt-8 items-center">
-        <p className="flex-1">Home</p>
-        <p className="flex-1">Services</p>
-        <p className="flex-1">Resources</p>
-        <p className="flex-1">Us</p>
-        <p className="flex-1">Contact info</p>
+      <div className="flex flex-1 items-center">
+        <p className="flex-1 text-center">Home</p>
+        <p className="flex-1 text-center">Services</p>
+        <p className="flex-1 text-center">Resources</p>
+        <p className="flex-1 text-center">Us</p>
+        <p className="flex-1 text-center">Contact info</p>  
       </div>
     </div>
   );
