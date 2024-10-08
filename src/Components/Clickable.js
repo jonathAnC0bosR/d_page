8c70c77eba68678c48d734825f1c74e57a2c2f3e
Jonathan Abraham Cobos Ramirez <jcobos@solarig.com>
2024-10-09 06:26:32 +0900

    Added clickable elements and erased the logo

--- src/Components/Clickable.js
+++ src/Components/Clickable.js
@@ -2,11 +2,11 @@ import {motion} from "framer-motion"
 
 export default function Clickable({ text }) {
     return (
-        <div className="text-white">
+        <div className="text-white flex justify-center h-fit">
             <motion.button 
             whileHover={{scale: 1.1}}
             whileTap={{scale: 0.9}}
-            className="p-8 bg-slate-700 rounded-2xl ">
+            className="p-8 bg-indigo-500 rounded-2xl">
                 {text}
             </motion.button>
         </div>
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,20 +1,24 @@
-import Clickable from "./Clickable"
-import g_logo from "../g_logo.jpg"
+import Clickable from "./Clickable";
+import g_logo from "../dron_T.png";
 
 export default function Landing() {
-    return (<div className="lg:flex lg:h-screen ">
-        <div className="sm:flex-1 justify-center grid grid-rows-2">
-            <div className="flex flex-1">
-                <h1 className="sm:text-6xl text-4xl px-10 text-white font-extrabold mt-20 text-center pb-5">Boost your solar plant production </h1>
-            </div>
-            <div className="sm:flex sm:flex-1 justify-center">
-                <div className="flex flex-1 justify-center"><Clickable text={'Request a quotation'} /></div>
-                <div className="flex flex-1 justify-center"><Clickable text={'Download a sample'} /></div>
-            </div>
+  return (
+    <div className="lg:flex lg:h-screen ">
+      <div className="sm:flex-1 justify-center grid grid-rows-2">
+        <div className="flex-1 grid grid-rows-2">
+          <h1 className="sm:text-6xl text-4xl px-10 text-white font-extrabold self-center text-center">
+            Boost your solar plant production{" "}
+          </h1>
+          <div className="grid grid-cols-2">
+            <Clickable  text={"Request a quotation"} />
+            <Clickable text={"Download a sample"} />
+          </div>
         </div>
-        <div className="lg:flex-1 lg:flex justify-center flex">
-            <img src={g_logo} alt="logo" className="w-4/6 h-4/6 rounded-3xl " />
-        </div>
+        <div className="sm:flex sm:flex-1 justify-center"></div>
+      </div>
+      <div className="lg:flex-1 lg:flex justify-center flex">
+        <img src={g_logo} alt="logo" className="w-fit h-fit py-3 rounded-3xl " />
+      </div>
     </div>
-    )
+  );
 }
