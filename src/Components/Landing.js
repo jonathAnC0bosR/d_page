6aad16ce07eb3155478530530c1975b753e1fbff
Jonathan Abraham Cobos Ramirez <jonathan.cobos@outlook.es>
2024-10-10 19:39:54 -0600

    Rectified dependencies

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,11 +1,13 @@
 import Clickable from "./Clickable"
 import g_logo from "../g_logo.jpg"
+import Typed from "react-typed"
 
 export default function Landing() {
     return (<div className="lg:flex lg:h-screen ">
         <div className="sm:flex-1 justify-center grid grid-rows-2">
             <div className="flex flex-1">
                 <h1 className="sm:text-6xl text-4xl px-10 text-white font-extrabold mt-20 text-center pb-5">Boost your solar plant production </h1>
+                <Typed />
             </div>
             <div className="sm:flex sm:flex-1 justify-center">
                 <div className="flex flex-1 justify-center"><Clickable text={'Request a quotation'} /></div>
