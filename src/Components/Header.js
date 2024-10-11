e63ca9c08baf07899a1c80e9823cd309ce91c304
Jonathan Abraham Cobos Ramirez <jonathan.cobos@outlook.es>
2024-10-10 20:37:40 -0600

    Fixed conflict merges and styled the landing, now start on middle part of the page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
-import Link from './Link'; 
+import Link from './Link';
+import g_logo from '../g_logo.jpg'
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,12 +11,16 @@ export default function Header() {
   };
 
   return (
-    <div>
-      <div className="relative flex py-3 text-white">
-        <div className="lg:flex lg:flex-1 lg:px-8">
-          <h1 className="flex px-4 text-2xl font-bold">
+    <>
+      <div className="flex py-3 text-white">
+        <div className="flex lg:flex-1 px-14 items-center ">
+          <div>
+            <img src={g_logo} alt='G_logo' className='h-10 w-10' />
+          </div>
+          <h1 className="lg:flex px-4 text-2xl font-bold">
             PVMEX
           </h1>
+
         </div>
         <div className="lg:flex lg:flex-1 lg:items-center hidden">
           <p className="flex-1 text-center relative after:content-[''] after:block after:w-16 after:left-1/2 after:translate-x-[-50%] after:h-[2px] after:bg-white after:absolute after:bottom-[-2px]">
@@ -36,11 +41,11 @@ export default function Header() {
         <div className="grid gap-1 bg-white text-black mx-1  p-4 rounded-md shadow-lg lg:hidden ">
           <p className='hover:font-extrabold'>Home</p>
           <p className='hover:font-extrabold'>Services</p>
-          <p>Resources</p>
-          <p>Us</p>
+          <p className='hover:font-extrabold'>Resources</p>
+          <p className='hover:font-extrabold'>Us</p>
           <p>Contact</p>
         </div>
       )}
-    </div>
+    </>
   );
 }
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,27 +1,24 @@
 import Clickable from "./Clickable"
-import g_logo from "../g_logo.jpg"
-import Typed from "react-typed"
+// import g_logo from "../g_logo.jpg"
+import t_img from "../dron_T.png"
 
 export default function Landing() {
-  return (<div className="lg:flex lg:h-screen ">
-        <div className="sm:flex-1 justify-center grid grid-rows-2">
-            <div className="flex flex-1">
-                <h1 className="sm:text-6xl text-4xl px-10 text-white font-extrabold mt-20 text-center pb-5">Boost your solar plant production </h1>
-                <Typed />
-            </div>
-            <div className="sm:flex sm:flex-1 justify-center">
-                <div className="flex flex-1 justify-center"><Clickable text={'Request a quotation'} /></div>
-                <div className="flex flex-1 justify-center"><Clickable text={'Download a sample'} /></div>
-            </div>
-        </div>
-        <div className="lg:flex-1 lg:flex justify-center flex">
-            <img src={g_logo} alt="logo" className="w-4/6 h-4/6 rounded-3xl " />
-        </div>
-        <div className="sm:flex sm:flex-1 justify-center"></div>
+  return (<div className="lg:flex lg:h-screen text-white ">
+    <div className="sm:flex-1 justify-center grid grid-rows-2">
+      <div className="mx-4 px-10 place-self-center">
+        <h1 className="sm:text-6xl text-4xltext-white font-extrabold">Boost your solar plant production </h1>
+        <p className="mt-5">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate eget, eros nisl aenean scelerisque dui facilisi euismod enim, himenaeos curabitur sem sapien cubilia odio porta class.
+        </p>
       </div>
-      <div className="lg:flex-1 lg:flex justify-center flex">
-        <img src={g_logo} alt="logo" className="w-fit h-fit py-3 rounded-3xl " />
+      <div className="sm:flex sm:flex-1">
+        <div className="flex flex-1 justify-center"><Clickable text={'Request a quotation'} /></div>
+        <div className="flex flex-1 justify-center"><Clickable text={'Download a sample'} /></div>
       </div>
-    </div >
+    </div>
+    <div className="lg:flex-1 lg:flex justify-center flex">
+      <img src={t_img} alt="logo" className="w-fit h-fit rounded-3xl " />
+    </div>
+  </div>
   );
 }
