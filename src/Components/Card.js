5bc0864d6c886044a0fd3b64c72343e1a0880edf
Jonathan Abraham Cobos Ramirez <jcobos@solarig.com>
2024-10-12 11:16:27 +0900

    Added group class to card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,6 @@
 export default function Card ({title, text}) {
     return (
-        <div className="text-center h-64 m-4 hover:bg-indigo-400 hover:rounded-2xl hover:text-white">
+        <div className="text-center group h-64 m-4 hover:bg-indigo-400 hover:rounded-2xl hover:text-white">
             <h1 className="p-3">
                 {title}
             </h1>
