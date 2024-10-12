f097f76cf2bb2354065155069292bd8c2fdceb7d
Jonathan Abraham Cobos Ramirez <jonathan.cobos@outlook.es>
2024-10-12 13:53:54 -0600

    Ended styling cards, now working on the logic behind the icons

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,15 @@
+import {GiDeliveryDrone} from "react-icons/gi"
+
 export default function Card ({title, text}) {
     return (
-        <div className="text-center group h-64 m-4 hover:bg-indigo-400 hover:rounded-2xl hover:text-white">
-            <h1 className="p-3">
-                {title}
+        <div className="group h-64 m-4 hover:bg-indigo-300 hover:rounded-xl text-center pt-5">
+            <div className="p-4 mx-auto text-sky-600 text-4xl rounded-full bg-sky-100 w-fit group-hover:bg-white">
+                <GiDeliveryDrone/>
+            </div>
+            <h1 className="p-3 group-hover:text-white group-hover:font-bold">
+                {title} 
             </h1>
-            <p className="text-gray-400 px-4 hover:text-white ">
+            <p className="text-gray-400 px-4 group-hover:text-white">
                 {text}
             </p>
         </div>
