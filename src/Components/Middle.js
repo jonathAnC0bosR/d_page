eb5a87e7cafe27f131ac913b4cab3298a3518f29
Jonathan Abraham Cobos Ramirez <jonathan.cobos@outlook.es>
2024-10-17 22:18:40 -0600

    Added carroulsel

--- src/Components/Middle.js
+++ src/Components/Middle.js
@@ -1,5 +1,18 @@
+import img_1 from "../Images/img_1.jpeg"
+import img_2 from "../Images/img_2.jpeg"
+import img_3 from "../Images/img_3.jpeg"
+import img_4 from "../Images/img_4.jpeg"
 import { motion } from "framer-motion"
-import Card from "./Card"
+import Carrousel from "./Carrousel"
+
+const slides = [
+    img_1,
+    img_2,
+    img_3,
+    img_4
+]
+// import Card from "./Card"
+
 
 export default function Middle() {
     return (
@@ -9,26 +22,33 @@ export default function Middle() {
                     OUR SERVICES
                 </h2>
                 <h1 className="text-5xl py-3">
-                    We provide a lot of cool services!
+                    We provide a lot of cool services
                 </h1>
                 <p className="text-gray-400">
                     Eros duis nisi justo sollicitudin molestie interdum sodales primis praesent sociis, fermentum consequat pharetra eleifend aenean mauris orci lacus viverra sociosqu, leo phasellus tempus sapien maecenas nunc ornare class rutrum.
                 </p>
                 <motion.button
                     className="place-self-start p-3 mt-3 bg-blue-600 rounded-2xl text-white hover:bg-blue-400"
-                    whileHover={{scale:1.1}}
-                    whileTap={{scale:0.9}}
-                    >
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
                     Our services
                 </motion.button>
             </div>
-            <div className="lg:flex-1 lg:grid gap-3 sm:grid-cols-3 lg:grid-rows-2 items-center grid grid-rows-5">
+            {/* <div className="lg:flex-1 lg:grid gap-3 sm:grid-cols-3 lg:grid-rows-2 items-center grid grid-rows-5">
                 <Card title={"Termography"} text={"Lorem ipsum dolor sit amet consectetur, adipiscing elit eros fringilla."} />
                 <Card title={"Reports"} text={"Lorem ipsum dolor sit amet consectetur, adipiscing elit eget convallis."} />
                 <Card title={"Monitoring"} text={"Lorem ipsum dolor sit amet consectetur, adipiscing elit natoque erat."}/>
                 <Card title={"Analysis"} text={"Lorem ipsum dolor sit amet consectetur, adipiscing elit interdum vestibulum."} />
                 <Card title={"AI"} text={"Lorem ipsum dolor sit amet consectetur, adipiscing elit commodo varius."} />
                 <Card title={"Debugging"} text={"Lorem ipsum dolor sit amet consectetur, adipiscing elit aenean venenatis."} />
+            </div> */}
+            <div className="max-w-lg">
+                <Carrousel autoSlide={false}>
+                    {slides.map((i) => (
+                        <img src={i} alt="img" className="" />
+                    ))}
+                </Carrousel>
             </div>
         </div>
     )
